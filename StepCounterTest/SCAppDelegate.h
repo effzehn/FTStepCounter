074dd5ce1112631356b03eba14f2c193499644e3
Andre Hoffmann <a.hoffmann@ahoffmann-rMBP-2.local>
2014-02-17 09:49:48 +0100

    Initial Commit

---
+++ StepCounterTest/SCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SCAppDelegate.h
+//  StepCounterTest
+//
+//  Created by Andre Hoffmann on 17.02.14.
+//  Copyright (c) 2014 welldoo GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StepCounterTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StepCounterTest
+//
+//  Created by Andre Hoffmann on 17.02.14.
+//  Copyright (c) 2014 welldoo GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([SCAppDelegate class]));
+	}
+}
